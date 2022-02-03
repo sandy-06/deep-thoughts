6e96f25688f645de7a187495a48fb67279e80f1c
Sandra Sanders <sandy-sanders@msn.com>
2022-02-03 14:04:20 -0700

    21.2

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,58 +1,51 @@
-//import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-//create out typeDefs
 const typeDefs = gql`
-
-type Thought{
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-}
-
-type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
-}
-
-type User {
+  type User {
     _id: ID
     username: String
     email: String
     friendCount: Int
     thoughts: [Thought]
     friends: [User]
-}
+  }
 
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
 
-type Query {
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
     me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
-}
+  }
 
-type Auth {
-    token: ID!
-    user: User
-}
-
-
-
-type Mutation {
+  type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-}
-
-
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
 `;
 
-
-//export the typeDefs
 module.exports = typeDefs;
