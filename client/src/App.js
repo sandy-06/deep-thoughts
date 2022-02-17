fc5427b54a92f4a16b314bc86d810e2cb6c70981
Sandra Sanders <sandy-sanders@msn.com>
2022-02-17 13:19:29 -0700

    21.5.4

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
